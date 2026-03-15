c362ea04470a86adbaeeb88dc8f752452a06928d
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 14:26:37 +0200

    Update stg_high_education.sql

--- 02_staging/stg_high_education.sql
+++ 02_staging/stg_high_education.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_high_education` AS
 WITH raw_split AS (
   SELECT
-    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(2)]) AS sector_code,
-    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(3)]) AS isced_level,
-    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(4)]) AS country_code,
+    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(3)]) AS sector_code,  -- PRV jne
+    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(5)]) AS isced_level,  -- ED5 jne
+    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(6)]) AS country_code, -- geo (AL, AT jne)
     * EXCEPT(string_field_0)
   FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.land_high_education`
   WHERE string_field_0 NOT LIKE '%TIME_PERIOD%'
@@ -24,24 +24,21 @@ SELECT
   isced_level,
   CASE 
     WHEN raw_field = 'string_field_1' THEN 2005
-    WHEN raw_field = 'string_field_2' THEN 2006
-    WHEN raw_field = 'string_field_3' THEN 2007
-    WHEN raw_field = 'string_field_4' THEN 2008
-    WHEN raw_field = 'string_field_5' THEN 2009
-    WHEN raw_field = 'string_field_6' THEN 2010
-    WHEN raw_field = 'string_field_7' THEN 2011
-    WHEN raw_field = 'string_field_8' THEN 2012
-    WHEN raw_field = 'string_field_9' THEN 2013
-    WHEN raw_field = 'string_field_10' THEN 2014
-    WHEN raw_field = 'string_field_11' THEN 2015
-    WHEN raw_field = 'string_field_12' THEN 2016
-    WHEN raw_field = 'string_field_13' THEN 2017
-    WHEN raw_field = 'string_field_14' THEN 2018
-    WHEN raw_field = 'string_field_15' THEN 2019
-    WHEN raw_field = 'string_field_16' THEN 2020
-    WHEN raw_field = 'string_field_17' THEN 2021
-    WHEN raw_field = 'string_field_18' THEN 2022
+    WHEN raw_field = 'string_field_2' THEN 2010
+    WHEN raw_field = 'string_field_3' THEN 2012
+    WHEN raw_field = 'string_field_4' THEN 2013
+    WHEN raw_field = 'string_field_5' THEN 2014
+    WHEN raw_field = 'string_field_6' THEN 2015
+    WHEN raw_field = 'string_field_7' THEN 2016
+    WHEN raw_field = 'string_field_8' THEN 2017
+    WHEN raw_field = 'string_field_9' THEN 2018
+    WHEN raw_field = 'string_field_10' THEN 2019
+    WHEN raw_field = 'string_field_11' THEN 2020
+    WHEN raw_field = 'string_field_12' THEN 2021
+    WHEN raw_field = 'string_field_13' THEN 2022
+    WHEN raw_field = 'string_field_14' THEN 2023
+    WHEN raw_field = 'string_field_15' THEN 2024
   END AS year,
   SAFE_CAST(REGEXP_REPLACE(value, r'[^0-9]', '') AS INT64) AS student_count
 FROM unpivoted
-WHERE value NOT LIKE '%:%';
+WHERE value NOT LIKE '%:%' AND value IS NOT NULL;
