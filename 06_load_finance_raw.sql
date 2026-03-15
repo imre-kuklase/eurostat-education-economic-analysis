1ee929991ef30321126a9955571384f03e3191dd
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 11:47:18 +0200

    Create 06_load_finance_raw.sql

---
+++ 06_load_finance_raw.sql
@@ -0,0 +1,9 @@
+-- Finantsandmete toorandmete laadimine Google Cloud Storage bucketist
+LOAD DATA OVERWRITE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.eurostat_finance_raw`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://kursusetoo/estat_educ_uoe_fine01.tsv'],
+  field_delimiter = '\t',
+  skip_leading_rows = 1,
+  allow_jagged_rows = true
+);
