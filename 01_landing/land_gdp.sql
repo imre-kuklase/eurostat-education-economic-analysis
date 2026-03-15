8c6c571b936c0adf58049a80810022245abd606f
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 16:34:50 +0200

    Update land_gdp.sql

--- 01_landing/land_gdp.sql
+++ 01_landing/land_gdp.sql
@@ -1,3 +1,4 @@
+-- SKP toorandmete laadimine Google Cloud Storage bucketist
 LOAD DATA OVERWRITE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.land_gdp`
 FROM FILES (
   format = 'CSV',
