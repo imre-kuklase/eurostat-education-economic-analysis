74e03aedc2452394ce280b9f8d99ee35be55c758
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 16:34:05 +0200

    Create land_gdp.sql

---
+++ 01_landing/land_gdp.sql
@@ -0,0 +1,8 @@
+LOAD DATA OVERWRITE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.land_gdp`
+FROM FILES (
+  format = 'CSV',
+  uris = ['gs://kursusetoo/estat_nama_10_gdp.tsv'],
+  field_delimiter = '\t',
+  skip_leading_rows = 1,
+  allow_jagged_rows = true
+);
