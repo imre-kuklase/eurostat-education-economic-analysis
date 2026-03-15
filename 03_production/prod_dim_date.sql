f89f2cdc828538259df170772b2158576d3a03dd
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 19:33:39 +0200

    Update prod_dim_date.sql

--- 03_production/prod_dim_date.sql
+++ 03_production/prod_dim_date.sql
@@ -1,4 +1,9 @@
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_dim_date` AS
 SELECT 
-  year
-FROM UNNEST(GENERATE_ARRAY(2005, 2025)) AS year;
+  year,
+  CASE 
+    WHEN year <= 2015 THEN '2012-2015'
+    WHEN year <= 2020 THEN '2016-2020'
+    ELSE '2021-2024'
+  END AS year_period
+FROM UNNEST(GENERATE_ARRAY(2012, 2024)) AS year;
