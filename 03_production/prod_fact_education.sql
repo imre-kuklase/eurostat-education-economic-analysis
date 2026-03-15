90bd77d188246c7f5bb7619416950f2f2dc251bc
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 19:43:41 +0200

    Update prod_fact_education.sql

--- 03_production/prod_fact_education.sql
+++ 03_production/prod_fact_education.sql
@@ -1,4 +1,17 @@
+/*
+  TABEL: prod_fact_education
+  KIRJELDUS: Üliõpilaste arv kõrghariduses.
+  ALLIKAS: stg_high_education
+  FILTER: 2012-2024, ainult positiivsed väärtused.
+*/
+
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_fact_education` AS
-SELECT country_code, sector_code, isced_level, year, student_count
+SELECT 
+  country_code, -- FK: prod_dim_country
+  sector_code,  -- FK: prod_dim_sector
+  isced_level,  -- FK: prod_dim_isced
+  year,         -- FK: prod_dim_date
+  student_count -- Mõõdik: õppurite arv
 FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_high_education`
-WHERE student_count > 0;
+WHERE year BETWEEN 2012 AND 2024 
+  AND student_count > 0;
