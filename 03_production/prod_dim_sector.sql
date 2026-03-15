44655d919ede7ad8fb09b854b58af89a81283488
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 14:38:20 +0200

    Update prod_dim_sector.sql

--- 03_production/prod_dim_sector.sql
+++ 03_production/prod_dim_sector.sql
@@ -1,39 +1,22 @@
--- Luuakse või asendatakse õppeasutuse sektori dimensioonitabel
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_dim_sector` AS
-SELECT DISTINCT 
-  sector_code, -- Algne kood faktitabelist (nt PUB, PRV_IND)
-  
-  -- Sektori koodi tõlkimine eestikeelseks nimetuseks
-  CASE 
-    WHEN sector_code = 'PUB' THEN 'Avalik-õiguslik'
-    WHEN sector_code = 'PRV' THEN 'Eraõiguslik (kokku)'
-    WHEN sector_code = 'PRV_DEP' THEN 'Riigist sõltuv eraõiguslik'
-    WHEN sector_code = 'PRV_IND' THEN 'Sõltumatu eraõiguslik'
-    WHEN sector_code = 'TOT_SEC' THEN 'Kõik sektorid kokku'
-    ELSE sector_code 
-  END AS sector_name_et,
+SELECT * FROM UNNEST([
+  -- Hariduse koodid (stg_high_education)
+  STRUCT(
+    'PUB' AS sector_code, 
+    'Avalikud õppeasutused' AS sector_name_et, 
+    'Public institutions' AS sector_name_en, 
+    'Avalik sektor' AS sector_group_et, 
+    'Public sector' AS sector_group_en
+  ),
+  ('PRV', 'Eraõppeasutused', 'Private institutions', 'Eerasektor', 'Private sector'),
+  ('PRV_DEP', 'Erasektori sõltuvad asutused', 'Private government-dependent', 'Eerasektor', 'Private sector'),
+  ('PRV_IND', 'Erasektori sõltumatud asutused', 'Private independent', 'Eerasektor', 'Private sector'),
+  ('TOT_SEC', 'Kõik sektorid kokku', 'Total sectors', 'Kokku', 'Total'),
 
-  -- Sektori koodi tõlkimine ingliskeelseks nimetuseks
-  CASE 
-    WHEN sector_code = 'PUB' THEN 'Public'
-    WHEN sector_code = 'PRV' THEN 'Private (total)'
-    WHEN sector_code = 'PRV_DEP' THEN 'Government-dependent private'
-    WHEN sector_code = 'PRV_IND' THEN 'Independent private'
-    WHEN sector_code = 'TOT_SEC' THEN 'Total sectors'
-    ELSE sector_code 
-  END AS sector_name_en,
-
-  -- Sektorite grupeerimine analüüsiks (Erasektor vs Avalik sektor)
-  CASE 
-    WHEN sector_code IN ('PRV', 'PRV_DEP', 'PRV_IND') THEN 'Erasektor'
-    WHEN sector_code = 'PUB' THEN 'Avalik sektor'
-    ELSE 'Muu/Agregeeritud'
-  END AS sector_group_et,
-
-  -- Sektorite grupeerimine inglise keeles
-  CASE 
-    WHEN sector_code IN ('PRV', 'PRV_DEP', 'PRV_IND') THEN 'Private Sector'
-    WHEN sector_code = 'PUB' THEN 'Public Sector'
-    ELSE 'Other/Aggregate'
-  END AS sector_group_en
-FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.high_education_final`;
+  -- Finantsi koodid (stg_finance)
+  ('S13', 'Valitsemissektor', 'General government', 'Avalik sektor', 'Public sector'),
+  ('S1', 'Kogu majandus', 'Total economy', 'Kokku', 'Total'),
+  ('S1D', 'Muud kodumaised sektorid', 'Other domestic sectors', 'Eerasektor', 'Private sector'),
+  ('S1D_OTH', 'Muud erasektori üksused', 'Other private entities', 'Eerasektor', 'Private sector'),
+  ('ORG_INTL', 'Rahvusvahelised organisatsioonid', 'International organisations', 'Määramata', 'Other')
+]);
