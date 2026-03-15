d32cfc7e7dde91f9e0fdae72a9f5658a8220ebfe
imre-kuklase <imre.kuklase@gmail.com>
2026-03-15 13:05:12 +0200

    Update land_finance.sql

--- 01_landing/land_finance.sql
+++ 01_landing/land_finance.sql
@@ -1,5 +1,5 @@
 -- Finantsandmete toorandmete laadimine Google Cloud Storage bucketist
-LOAD DATA OVERWRITE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.eurostat_finance_raw`
+LOAD DATA OVERWRITE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.land_finance`
 FROM FILES (
   format = 'CSV',
   uris = ['gs://kursusetoo/estat_educ_uoe_fine01.tsv'],
