285ac9e7c32dc1c83bff1e985683821e777510b1
imre-kuklase <imre.kuklase@gmail.com>
2026-03-16 22:31:04 +0200

    Update prod_fact_education.sql

--- 03_production/prod_fact_education.sql
+++ 03_production/prod_fact_education.sql
@@ -1,17 +1,32 @@
+/*
+  TABEL: prod_fact_education
+  KIRJELDUS: Õppurite jahunemine riigiti era- ja avaliku sektori kaupa
+  ALLIKAS: stg_high_education
+  FILTER: 2012-2022, sünkroniseeritud teiste faktitabelitega.
+*/
+
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_fact_education` AS
 SELECT 
-    country_code,
-    sector_code,
-    isced_level,
-    year,
-    sex_code,
-    worktime_code,
-    unit_code,
-    student_count
+  country_code,
+  isced_level,
+  sector_code,
+  year,
+  student_count
 FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_high_education`
-WHERE student_count IS NOT NULL
-  AND sex_code != 'T' -- 1. Eemaldame soo koondsummad (jäävad M ja F)
-  AND worktime_code NOT IN ('Total', 'TOT_FTE') -- 2. Eemaldame töökoormuse koondsummad (jäävad FT ja PT)
-  AND sector_code NOT IN ('TOT_SEC', 'PRV_IND') -- 3. Eemaldame sektorite koondsummad (jäävad PUB, PRV jne)
-  AND isced_level IN ('ED5', 'ED6', 'ED7', 'ED8') -- 4. Eemaldame haridustasemete koondsummad (jäävad ED5, ED6, ED7, ED8)
-  AND unit_code = 'NR'; -- 5. Veendume, et ühik on arvuline (Number)
+WHERE
+  -- Regiooni koondridade välistamine
+  country_code NOT LIKE 'EU%'
+  -- Filtreeritud ainult kõrghariduse tasemed
+  AND isced_level IN ('ED5','ED6','ED7','ED8')
+  -- Fookus: Avalik vs Erasektor
+  AND sector_code IN ('PUB','PRV')
+  -- Granulaarsus lukustatud koondsummadele
+  AND sex_code = 'T'
+  AND worktime_code = 'TOTAL'
+  -- Uurimustöö ajaline raam
+  AND year BETWEEN 2012 AND 2022
+ORDER BY
+  year,
+  country_code,
+  isced_level,
+  sector_code
