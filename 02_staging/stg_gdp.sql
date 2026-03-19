e5e1ccf13532f3d9cbbf03e5b4677ccf604237c0
imre-kuklase <imre.kuklase@gmail.com>
2026-03-19 11:41:21 +0200

    Update stg_gdp.sql

--- 02_staging/stg_gdp.sql
+++ 02_staging/stg_gdp.sql
@@ -2,14 +2,15 @@
   SKRIPT: stg_gdp.sql
   EESMÄRK: SKP toorandmete puhastamine ja normaliseerimine.
   TRANSFORMATSIOONID:
-    1. SPLIT: Eraldame esimesest tulpast ühiku, indikaatori ja riigikoodi.
-    2. UNPIVOT: Teisendame aastate veerud (1975-2024) ridadeks.
+    1. SPLIT: Eraldame metadata (freq,unit,na_item,geo).
+    2. UNPIVOT: Teisendame aastate veerud ridadeks.
     3. REGEXP: Eemaldame Eurostati märkmed (lipud b, p, d) numbrilistest väärtustest.
 */
 
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_gdp` AS
 WITH raw_split AS (
   SELECT
+    TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(0)]) AS freq,
     TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(1)]) AS unit,
     TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(2)]) AS na_item,
     TRIM(SPLIT(string_field_0, ',')[SAFE_OFFSET(3)]) AS country_code,
@@ -29,6 +30,7 @@ unpivoted AS (
 )
 SELECT 
   country_code,
+  freq,
   unit,
   na_item,
   -- Dünaamiline aasta arvutus: string_field_36 vastab aastale 2010
@@ -36,4 +38,6 @@ SELECT
   -- Puhastame väärtuse: eemaldame kõik peale numbrite ja punkti
   SAFE_CAST(REGEXP_REPLACE(value, r'[^0-9.]', '') AS FLOAT64) AS gdp_amount
 FROM unpivoted
-WHERE value NOT LIKE '%:%' AND value IS NOT NULL; -- Eemaldame puuduvad andmed
+WHERE 
+  value NOT LIKE '%:%' 
+  AND value IS NOT NULL; -- Eemaldame puuduvad andmed
