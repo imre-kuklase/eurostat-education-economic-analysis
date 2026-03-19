24d691c5fdf7069a70b5d7533fe731e081245971
imre-kuklase <imre.kuklase@gmail.com>
2026-03-19 11:51:14 +0200

    Update prod_fact_gdp.sql

--- 03_production/prod_fact_gdp.sql
+++ 03_production/prod_fact_gdp.sql
@@ -2,18 +2,18 @@
   TABEL: prod_fact_gdp
   KIRJELDUS: Riikide SKP näitajad jooksvates hindades (Current Prices).
   ALLIKAS: stg_gdp
-  AJARAAM: 2012-2024 (Sünkroniseeritud haridusandmetega)
+  AJARAAM: 2012-2022 (Sünkroniseeritud haridusandmetega)
 */
 
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_fact_gdp` AS
 SELECT 
   country_code,
   year,
-  unit AS unit_code,  -- Kasutame ainult jooksvates hindades (miljonites eurodes) mõõdikut, et tagada võrreldavus hariduskulude tabeliga. 
-  na_item, -- B1GQ tähistab SKP-d turuhindades (Eurostati standard).
+  unit AS unit_code,  
+  na_item, 
   gdp_amount
 FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_gdp`
 WHERE year BETWEEN 2012 AND 2022 
-  AND unit = 'CP_MEUR' 
-  AND na_item = 'B1GQ'
+  AND unit = 'CP_MEUR' -- Kasutame ainult jooksvates hindades (miljonites eurodes) mõõdikut, et tagada võrreldavus hariduskulude tabeliga.
+  AND na_item = 'B1GQ' -- B1GQ tähistab SKP-d turuhindades (Eurostati standard).
   AND gdp_amount > 0; -- Välistame puuduvad või vigased kirjed
