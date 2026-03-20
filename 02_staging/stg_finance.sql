872408b37056c7809d06059425a1ce594b973006
imre-kuklase <imre.kuklase@gmail.com>
2026-03-20 09:43:11 +0200

    Update stg_finance.sql

--- 02_staging/stg_finance.sql
+++ 02_staging/stg_finance.sql
@@ -4,16 +4,16 @@
   TRANSFORMATSIOONID: 
     1. SPLIT: Eraldame metadata (freq, unit, sector, isced11, geo).
     2. UNPIVOT: Teisendame aastate veerud ridadeks.
-    3. REGEXP: Eemaldame Eurostati märkmed (nagu 'b' - break, 'e' - estimate).
+    3. REGEXP & EXTRACT: Eraldame Eurostati märkmed (lipud) väärtustest eraldi veergu 'flag_code'. Võimaldab hoida arvulised väärtused puhtana (FLOAT64) ja säilitada metaandmed analüüsiks.
         - 'b': Break in time series - Aegrea katkemine. Metoodika või andmeallikas on muutunud (Selgitab järske hüppeid graafikul)
         - 'p': Provisional - Esialgne. Need on värsked andmed, mis võivad hiljem korrigeeruda (Tavaliselt viimase aasta andmete juures)
         - 'd': Definition differs - Definitsioon erineb. Andmed ei vasta 100% standardile, aga on võrreldavad (Märguanne, et riik raporteerib midagi veidi teisiti)
         - 'e': Estimated - Hinnatud. Eurostat või riik on puuduva osa tuletanud mudelitega (Kasutatakse päris küsitlusandmete puudumisel)
         - 'u': Low reliability - Väike usaldusväärsus. Valimi suurus oli liiga väike (Tuleb olla ettevaatlik ridade tõlgendamisel)
-        - 'u': Confidential - Salastatud. Ärihuvide / riikliku julgeoleku või muu kaitseks andmete salastatamine. (nt. kui riigis on ainult üks erakool, võidakse andmed salastada, et mitte avaldada kooli ärisaladust)
+        - 'c': Confidential - Salastatud. Ärihuvide / riikliku julgeoleku või muu kaitseks andmete salastatamine. (nt. kui riigis on ainult üks erakool, võidakse andmed salastada, et mitte avaldada kooli ärisaladust)
         - 'n': Not significant - Statistiliselt mitteoluline. Väärtus on nii väike, et ümardub nulliks.
-        - 'z': Not applicable - Pole asjakohane. Viitab sellele, et kontseptsioon või andmepunkt ei ole antud riigi või sektori puhul üldse võimalik.
-        - ':': Missing value - Puuduv väärtus.
+        - 'z': Not applicable - Pole asjakohane / ei kohaldata. Viitab sellele, et kontseptsioon või andmepunkt ei ole antud riigi või sektori puhul üldse võimalik.
+        - ':': Missing value - Puuduv väärtus (filtreeritakse välja.
 */
 
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_finance` AS
@@ -57,6 +57,10 @@ SELECT
     WHEN raw_field = 'string_field_10' THEN 2021
     WHEN raw_field = 'string_field_11' THEN 2022
   END AS year,
-  SAFE_CAST(REGEXP_REPLACE(value, r'[^0-9.]', '') AS FLOAT64) AS expenditure_amount
+  -- 1. PUHAS NUMBER: eemaldame kõik peale numbrite ja punkti
+  SAFE_CAST(REGEXP_REPLACE(value, r'[^0-9.]', '') AS FLOAT64) AS expenditure_amount,
+  -- 2. LIPU KOOD: otsime üles kõik tähed (a-z) ja paneme need eraldi veergu
+  -- Kui tähte pole, jääb NULL (st. puhas andmepunkt)
+  TRIM(REGEXP_EXTRACT(value, r'[a-z]+')) AS flag_code
 FROM unpivoted
 WHERE value NOT LIKE '%:%';
