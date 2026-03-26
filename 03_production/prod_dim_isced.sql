74d05e947c60f981ba29fd82510e17ed7d9b5a19
imre-kuklase <imre.kuklase@gmail.com>
2026-03-26 17:53:58 +0200

    Update prod_dim_isced.sql

--- 03_production/prod_dim_isced.sql
+++ 03_production/prod_dim_isced.sql
@@ -1,13 +1,16 @@
 /*
   TABEL: prod_dim_isced
-  KIRJELDUS: Haridustasemed (ISCED 2011 standard) koos sorteerimisjärjekorraga. prod_dim tabelite loomisel kasutatud ainult ED5 - ED8 peamiseid kategooriaid.
+  KIRJELDUS: Haridustasemed koos agregaat-kategooriaga finantsanalüüsi jaoks.
 */
 
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_dim_isced` AS
 SELECT * FROM UNNEST([
-  -- KÕRGHARIDUS (ED5-ED8)
-  STRUCT('ED5' AS isced_level, 'Lühitsükli kõrgharidus' AS isced_name_et, 'Short-cycle tertiary' AS isced_name_en, 'Kõrgharidus' AS isced_group_et, 'Higher Education' AS isced_group_en, 1 AS sort_order),
-  ('ED6', 'Bakalaureuseõpe', 'Bachelor or equivalent', 'Kõrgharidus', 'Higher Education', 2),
-  ('ED7', 'Magistriõpe', 'Master or equivalent', 'Kõrgharidus', 'Higher Education', 3),
-  ('ED8', 'Doktoriõpe', 'Doctoral or equivalent', 'Kõrgharidus', 'Higher Education', 4)
+  -- KÕRGHARIDUSE AGREGAAT (Finantsandmete jaoks)
+  STRUCT('ED5-8' AS isced_level, 'Kõrgharidus (kokku)' AS isced_name_et, 'Tertiary education (total)' AS isced_name_en, 'Agregaat' AS level_type, 0 AS sort_order),
+  
+  -- DETAILSED TASEMED (Tudengite arvu analüüsi jaoks)
+  ('ED5', 'Lühitsükli kõrgharidus', 'Short-cycle tertiary', 'Detailne', 1),
+  ('ED6', 'Bakalaureuseõpe', 'Bachelor or equivalent', 'Detailne', 2),
+  ('ED7', 'Magistriõpe', 'Master or equivalent', 'Detailne', 3),
+  ('ED8', 'Doktoriõpe', 'Doctoral or equivalent', 'Detailne', 4)
 ]);
