c686cc434223bed3b1a918154c02cdfb282f3185
imre-kuklase <imre.kuklase@gmail.com>
2026-03-27 05:10:41 +0200

    Update prod_fact_gdp.sql

--- 03_production/prod_fact_gdp.sql
+++ 03_production/prod_fact_gdp.sql
@@ -15,7 +15,7 @@ SELECT
   flag_code
 FROM `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.stg_gdp`
 WHERE 
-  year BETWEEN 2012 AND 2022 
+  year BETWEEN 2013 AND 2022 
   AND unit = 'CP_MEUR' -- Kasutame ainult jooksvates hindades (miljonites eurodes) mõõdikut, et tagada võrreldavus hariduskulude tabeliga.
   AND na_item = 'B1GQ' -- B1GQ tähistab SKP-d turuhindades (Eurostati standard).
   AND gdp_amount > 0 -- Välistame puuduvad või vigased kirjed
