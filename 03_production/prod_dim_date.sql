8871f066d0b202041d2f85ac6d39dc6a5296507a
imre-kuklase <imre.kuklase@gmail.com>
2026-03-27 05:12:49 +0200

    Update prod_dim_date.sql

--- 03_production/prod_dim_date.sql
+++ 03_production/prod_dim_date.sql
@@ -1,15 +1,15 @@
 /*
   TABEL: prod_dim_date
-  KIRJELDUS: Ühtne ajatelg perioodiks 2012-2022.
+  KIRJELDUS: Ühtne ajatelg perioodiks 2013-2022.
 */
 
 CREATE OR REPLACE TABLE `optimal-cogency-483908-t3.kursusetoo_korghariduse_analyys.prod_dim_date` AS
 SELECT 
   year,
   CASE 
-    WHEN year <= 2015 THEN '2012-2015'
-    WHEN year <= 2020 THEN '2016-2020'
-    ELSE '2021-2024'
+    WHEN year <= 2015 THEN '2013-2017'
+    WHEN year <= 2020 THEN '2017-2020'
+    ELSE '2021-2022'
   END AS year_period
 FROM UNNEST(GENERATE_ARRAY(2012, 2022)) AS year
 ORDER BY
